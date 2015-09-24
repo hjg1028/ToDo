475371ccae1686b7ba9b54e67689984815a9acb5
hjg1028 <hjg1028@163.com>
2015-09-24 22:40:56 +0800

    add a local database

--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 var todos: [TodoModel] = []
 
+func dateFromString(dateStr: String) -> NSDate? {
+    let dateFormatter = NSDateFormatter()
+    dateFormatter.dateFormat = "yyyy-mm-dd"
+    let date = dateFormatter.dateFromString(dateStr)
+    return date
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        todos = [TodoModel(id: "1", image: "a.png", title: "买菜", date: dateFromString("2014-10-28")!), TodoModel(id: "2", image: "a.png", title: "买菜", date: dateFromString("2014-10-28")!), TodoModel(id: "3", image: "a.png", title: "买菜", date: dateFromString("2014-10-28")!), TodoModel(id: "4", image: "a.png", title: "买菜", date: dateFromString("2014-10-28")!)]
     }
 
     override func didReceiveMemoryWarning() {
